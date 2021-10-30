64b9a6f8cab314d0919cef7a61476f93d3d80ec5
mirshahbazi <mr.mirshahbazi@gmail.com>
2021-10-30 13:13:16 +0330

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MainPageState extends State<MainPage> {
                   textStyle: TextStyle(fontSize: 20),
                 ),
                 onPressed: showSheet,
-                child: Text('Open S tyle Sheet'))),
+                child: Text('Open Style Sheet'))),
       );
 
   Future showSheet() => showSlidingBottomSheet(context,
